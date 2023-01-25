5f518d290b181aa83a22f99b778c113c348f0acf
tracer-nju <tracer@nju.edu.cn>
2023-01-25 11:51:15 +0800

    Linux thomation 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:51:15 up  1:18,  0 users,  load average: 0.05, 0.04, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
+#include <dirent.h>
 
+void travel_proc()
+{
+
+}
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
+    travel_proc();
   }
   assert(!argv[argc]);
   return 0;
