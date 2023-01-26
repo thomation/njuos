8f320300559ed1c18b50fdbee45dd37e99464966
thomation <thomation@126.com>
2023-01-26 10:25:44 +0800

    Read comm

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -9,6 +9,12 @@ enum Option
   OPTION_N = 0x1,
   OPTION_P = 0x2,
 };
+struct ProcessInfo
+{
+  unsigned int id;
+  char comm[100];
+};
+
 enum Option parse_option(char * option)
 {
     if(strcmp(option,"-p"))
@@ -29,6 +35,21 @@ int is_proc_dir(char * dir)
   }
   return 1;
 }
+int get_process_info(char * proc_dir, struct ProcessInfo * info)
+{
+  char filename[200];
+  strncpy(filename, proc_dir, 200);
+  strncat(filename,"/comm", 100);
+  FILE *fp = fopen(filename, "r");
+  if (fp) {
+    fscanf(fp, "%s", info->comm);
+    fclose(fp);
+  } else {
+    printf("Cannot open file: %s\n", filename);
+    return -1;
+  }
+  return 0;
+}
 void travel_proc(unsigned int option)
 {
   printf("travel_proc with option:%u\n", option);
@@ -38,7 +59,14 @@ void travel_proc(unsigned int option)
   while((item = readdir(proc)))
   {
     if(is_proc_dir(item->d_name) && item->d_type == 4)
-      printf("name:%s\n", item->d_name);
+    {
+      struct ProcessInfo info;
+      sscanf(item->d_name, "%u", &info.id);
+      char path[100] = "/proc/";
+      strncat(path, item->d_name, 90);
+      get_process_info(path, &info);
+      printf("id:%u, comm:%s\n", info.id, info.comm);
+    }
   }
   closedir(proc);
 }
