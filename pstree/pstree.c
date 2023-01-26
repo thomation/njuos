083f13c1441772bf751ea6fbb7c1ef4bdd192383
thomation <thomation@126.com>
2023-01-26 09:34:11 +0800

    parse option

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
 #include <dirent.h>
+#include <string.h>
 
-void travel_proc()
+enum Option
 {
+  NONE = 0x0,
+  OPTION_N = 0x1,
+  OPTION_P = 0x2,
+};
+enum Option parse_option(char * option)
+{
+    if(strcmp(option,"-p"))
+      return OPTION_P;
+    if(strcmp(option, "-n"))
+      return OPTION_N;
+    printf("unsupport option %sn", option);
+    return NONE;
+}
+void travel_proc(unsigned int option)
+{
+  printf("travel_proc with option:%u\n", option);
   DIR *proc = opendir("/proc");
   assert(proc);
   struct dirent * item = NULL;
@@ -11,13 +28,16 @@ void travel_proc()
   {
     printf("name:%s\n", item->d_name);
   }
+  closedir(proc);
 }
 int main(int argc, char *argv[]) {
+  unsigned int option = 0;
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
+    option |= parse_option(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
-    travel_proc();
   }
+  travel_proc(option);
   assert(!argv[argc]);
   return 0;
 }
