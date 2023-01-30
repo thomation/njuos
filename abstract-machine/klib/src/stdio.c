c309567b810a235481174047615b9a6531a687e1
thomation <thomation@126.com>
2023-01-30 08:44:06 +0800

    correct printf %d to avoid overflow

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,24 +3,43 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
+#define MAX_INT_STR_LEN 100
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int uint_to_str(unsigned int n, char * str)
 {
-  char tmp[100];
+  char tmp[MAX_INT_STR_LEN];
   int i = 0;
   while(n > 0)
   {
-    tmp[i] = n % 10 + '0';
+    tmp[i ++] = n % 10 + '0';
     n /= 10;
-    i ++;
   }
   int j = 0;
   while(i > 0)
   {
-    str[j] = tmp[--i];
-    j ++;
+    str[j++] = tmp[--i];
+  }
+  str[j] = '\0';
+  return j;
+}
+int int_to_str(int n, char * str)
+{
+  if(n > 0)
+    return uint_to_str(n, str);
+  char tmp[MAX_INT_STR_LEN];
+  int i = 0;
+  while(n != 0)
+  {
+    tmp[i++] = - (n % 10) + '0';
+    n /= 10;
+  }
+  str[0] = '-';
+  int j = 1;
+  while(i > 0)
+  {
+    str[j++] = tmp[--i];
   }
   str[j] = '\0';
   return j;
@@ -38,14 +57,8 @@ int printf(const char *fmt, ...) {
       if(*cur == 'd')
       {
         int i = va_arg(list, int);
-        unsigned int u = i > 0 ? i : -i;
-        char str[100];
-        int len = uint_to_str(u, str);
-        if(i < 0)
-        {
-          putch('-');
-          n ++;
-        }
+        char str[MAX_INT_STR_LEN];
+        int len = int_to_str(i, str);
         putstr(str);
         n += len;
         cur ++;
