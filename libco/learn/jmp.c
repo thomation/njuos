9e2f091461ada48bf432aa35f35f551b39109d87
Thomation <thomation@126.com>
2023-02-05 11:25:12 +0800

    Don't call return can fix crash

--- libco/learn/jmp.c
+++ libco/learn/jmp.c
@@ -1,23 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <setjmp.h>
 
+static jmp_buf context;
 static int method(void *param)
 {
-    printf("Hello jmp start\n");
-    int i = (int)param;
-    printf("Hello jmp param\n");
-    printf("Hello Jmp %d\n", i);
-    printf("Hello jmp return\n");
-    return 1;
+  printf("Hello jmp start\n");
+  int i = (int)param;
+  printf("Hello jmp param\n");
+  printf("Hello Jmp %d\n", i);
+  longjmp(context, 1);
+  printf("Hello jmp return\n");
+  return 1;
 }
-static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
+static void method_return()
+{
+  printf("method_return\n");
+  longjmp(context, 1);
+}
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg, void* exit)
 {
   asm volatile(
 #if __x86_64__
-      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      "movq %0, %%rsp; movq %3, 0x8(%%rsp); movq %2, %%rdi; jmp *%1"
       :
-      : "b"((uintptr_t)sp), "d"(entry), "a"(arg)
+      : "b"((uintptr_t)sp), "d"(entry), "a"(arg), "r"(exit)
 #else
       "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
       :
@@ -27,10 +35,18 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg)
 }
 int main()
 {
-    int size = sizeof(u_int8_t) * 8192;
-    u_int8_t *stack = malloc(size);
-    u_int8_t *top = stack + size - 8;
-    stack_switch_call(top, method, 7);
-    printf("Jmp end\n");
-    return 0;
+  int size = sizeof(u_int8_t) * 8192;
+  u_int8_t *stack = malloc(size);
+  u_int8_t *top = stack + size - 8;
+
+  int val = setjmp(context);
+  if (val == 0)
+  {
+    stack_switch_call(top, method, 7, method_return);
+  }
+  else
+  {
+  }
+  printf("Jmp end\n");
+  return 0;
 }
