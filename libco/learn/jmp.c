cd9099fb0e3f0e40dc2fbb3e4850e8ec4acf57ad
Thomation <thomation@126.com>
2023-02-08 08:29:21 +0800

    add test of jmp

--- libco/learn/jmp.c
+++ libco/learn/jmp.c
@@ -2,10 +2,25 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <setjmp.h>
-int main() {
-    int n = 0;
-    jmp_buf buf;
-    setjmp(buf);
-    printf("Hello %d\n", n);
-    longjmp(buf, n++);
+static jmp_buf buf;
+static inline void save()
+{
+  if(setjmp(buf) != 0)
+    printf("Comeback\n");
+}
+void restore()
+{
+  longjmp(buf, 1);
+}
+int main()
+{
+  int n = 0;
+  // setjmp(buf);
+  save();
+  n++;
+  // pc can be restored.
+  // call stack can be restored.
+  // but the value on stack will be the newest.
+  printf("Hello %d\n", n);
+  restore();
 }
