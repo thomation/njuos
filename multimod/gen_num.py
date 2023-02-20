cb488c20fe2dc73713a8711e6608052c6a7c5822
Thomation <thomation@126.com>
2023-02-20 10:52:29 +0800

    random test case

--- multimod/gen_num.py
+++ multimod/gen_num.py
@@ -1,8 +1,16 @@
+import random
+
 def print_num(a, b, m):
     r = a * b % m
     print(a, b, m, r)
 
+# special case
 print_num(123, 456, 789)
-print_num(456, 789, 123)
 print_num(123, 456, 2**64-1)
 print_num(2**64-2, 2**64-2, 2**64-1)
+
+for _ in range(1, 10):
+    a = random.randint(0, 2**64 - 1)
+    b = random.randint(0, 2**64 - 1)
+    m = random.randint(1, 2**64 - 1)
+    print_num(a, b, m)
