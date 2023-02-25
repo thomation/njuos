38b5bcfeaf8739df4b67b2c41280f4266d9e1296
Thomation <thomation@126.com>
2023-02-25 09:50:51 +0800

    Add bits unit64 convert functions

--- multimod/main.c
+++ multimod/main.c
@@ -1,19 +1,38 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <assert.h>
+
+#define U64 "%" PRIu64
+#define D64 "%" PRId64
+#define X64 "%" PRIx64
 
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 int64_t multimod_fast(int64_t a, int64_t b, int64_t m);
+void uint64_to_bits(uint64_t n, uint8_t *bits); 
+uint64_t bits_to_uint64(uint8_t * bits); 
 
+void print_bits(uint8_t * bits, int len) {
+  for(int i = 0; i < len; i ++)
+    printf("%c", bits[i] + '0');
+  printf("\n");
+}
 void test(uint64_t a, uint64_t b, uint64_t m, uint64_t r) {
   uint64_t v = multimod(a, b, m);
-  #define U64 "%" PRIu64
   printf(U64 " * " U64 " mod " U64 " = " U64 " ?= " U64 " --> %s \n", a, b, m, v, r,  v == r ? "pass" : "fail");
 }
 void test2(int64_t a, int64_t b, int64_t m, int64_t r) {
   int64_t v = multimod_fast(a, b, m);
-  #define D64 "%" PRId64
   printf(D64 " * " D64 " mod " D64 " = " D64 " ?= " D64 " --> %s \n", a, b, m, v, r,  v == r ? "pass" : "fail");
+}
+void test_bits_convert(uint64_t n) {
+  printf(X64 "\n", n);
+  uint8_t bits[64];
+  uint64_to_bits(n, bits);
+  print_bits(bits, 64);
+  uint64_t n2 = bits_to_uint64(bits);
+  printf(X64 "\n", n2);
+  assert(n == n2);
 }
 
 int main(int argc, char* argv[]) {
@@ -22,6 +41,10 @@ int main(int argc, char* argv[]) {
   sscanf(argv[2], U64, &b);
   sscanf(argv[3], U64, &m);
   sscanf(argv[4], U64, &r);
+  test_bits_convert(a);
+  test_bits_convert(b);
+  test_bits_convert(m);
+  test_bits_convert(r);
   test(a, b, m, r);
   // int64_t a, b, m, r;
   // sscanf(argv[1], D64, &a);
--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,6 +1,28 @@
 #include <stdint.h>
+#include <stdio.h>
+
+// 123 == 0x7b == 0000000000000000000000000000000000000000000000000000000001111011
+static uint8_t a_bits[64];
+static uint8_t b_bits[64];
+static uint8_t m_bits[64];
+void uint64_to_bits(uint64_t n, uint8_t *bits) {
+  for(int i = 0; i < 64; i ++) {
+    bits[64 - i - 1]  = (n >> i) & 0x1;
+  }
+}
+uint64_t bits_to_uint64(uint8_t * bits) {
+  uint64_t n = 0;
+  for(int i = 0; i < 64; i ++) {
+    n += bits[i] ? (uint64_t) 0x1 << (63 - i) : 0;
+  }
+  return n;
+}
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+  uint64_to_bits(a, a_bits);
+  uint64_to_bits(b, b_bits);
+  uint64_to_bits(m, m_bits);
+
   return (a * b) % m; // buggy
 }
 
