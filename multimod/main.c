87f146d062ffadc16be507123011fc7bd3de7d93
Thomation <thomation@126.com>
2023-02-26 11:15:28 +0800

    Add bits multiple

--- multimod/main.c
+++ multimod/main.c
@@ -10,14 +10,10 @@
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 int64_t multimod_fast(int64_t a, int64_t b, int64_t m);
 void uint64_to_bits(uint64_t n, uint8_t *bits); 
-uint64_t bits_to_uint64(uint8_t * bits); 
-
-void print_bits(uint8_t * bits, int len) {
-  for(int i = 0; i < len; i ++)
-    printf("%c", bits[i] + '0');
-  printf("\n");
-}
+uint64_t bits_to_uint64(uint8_t * bits, int len); 
+void print_bits(uint8_t * bits, int len);
 void test(uint64_t a, uint64_t b, uint64_t m, uint64_t r) {
+  printf(U64 " * " U64 " mod " U64 " = " U64 " \n", a, b, m, r);
   uint64_t v = multimod(a, b, m);
   printf(U64 " * " U64 " mod " U64 " = " U64 " ?= " U64 " --> %s \n", a, b, m, v, r,  v == r ? "pass" : "fail");
 }
@@ -30,7 +26,7 @@ void test_bits_convert(uint64_t n) {
   uint8_t bits[64];
   uint64_to_bits(n, bits);
   print_bits(bits, 64);
-  uint64_t n2 = bits_to_uint64(bits);
+  uint64_t n2 = bits_to_uint64(bits, 64);
   printf(X64 "\n", n2);
   assert(n == n2);
 }
@@ -41,10 +37,10 @@ int main(int argc, char* argv[]) {
   sscanf(argv[2], U64, &b);
   sscanf(argv[3], U64, &m);
   sscanf(argv[4], U64, &r);
-  test_bits_convert(a);
-  test_bits_convert(b);
-  test_bits_convert(m);
-  test_bits_convert(r);
+  // test_bits_convert(a);
+  // test_bits_convert(b);
+  // test_bits_convert(m);
+  // test_bits_convert(r);
   test(a, b, m, r);
   // int64_t a, b, m, r;
   // sscanf(argv[1], D64, &a);
--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,29 +1,102 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <assert.h>
 
+void print_bits(uint8_t * bits, int len) {
+  for(int i = 0; i < len; i ++)
+    printf("%c", bits[i] + '0');
+  printf("\n");
+}
 // 123 == 0x7b == 0000000000000000000000000000000000000000000000000000000001111011
-static uint8_t a_bits[64];
-static uint8_t b_bits[64];
-static uint8_t m_bits[64];
 void uint64_to_bits(uint64_t n, uint8_t *bits) {
   for(int i = 0; i < 64; i ++) {
     bits[64 - i - 1]  = (n >> i) & 0x1;
   }
 }
-uint64_t bits_to_uint64(uint8_t * bits) {
+uint64_t bits_to_uint64(uint8_t * bits, int len) {
+  assert(len <= 64);
   uint64_t n = 0;
-  for(int i = 0; i < 64; i ++) {
-    n += bits[i] ? (uint64_t) 0x1 << (63 - i) : 0;
+  for(int i = 0; i < len; i ++) {
+    n += bits[i] ? (uint64_t) 0x1 << (len - 1 - i) : 0;
   }
   return n;
 }
-
+void bit_multi_bits(uint8_t bit, uint8_t *bits, uint8_t * ret)
+{
+  for(int i = 0; i < 64; i ++) {
+    ret[i] = bit == 0x1 ? bits[i] : 0x0;
+  }
+}
+int bits_left_1(uint8_t *bits, int len) {
+  for(int i = 0; i < len; i ++) {
+    if(bits[i] == 1) 
+      return i;
+  }
+  assert(0);
+}
+void bits_sub(uint8_t *l_bits, int l_size, uint8_t *r_bits, int r_size) {
+  uint64_t l = bits_to_uint64(l_bits, l_size);
+  uint64_t r = bits_to_uint64(r_bits, r_size);
+  uint64_t t = l - r;
+  uint8_t t_bits[64];
+  uint64_to_bits(t, t_bits);
+  for(int i = 0; i < l_size; i ++)
+    l_bits[i] = t_bits[i + 64 - l_size];
+}
+uint64_t bits_mod(uint8_t *bits, uint8_t * m_bits) {
+  int left = bits_left_1(bits, 128);
+  int m_left = bits_left_1(m_bits, 64);
+  // printf("left:%d, m_left:%d\n", left, m_left);
+  // printf("m_left bits:%d\n",m_bits[m_left]);
+  for(int i = left; i < 128; i ++) {
+    if(128 - i < 64 - m_left)
+      break;
+    if(bits[i] == 0) {
+      continue;
+    }
+    // bits[i] == 1
+    int m_is_small = 1;
+    for(int j = m_left; j < 64; j ++) {
+      if(bits[i + j - m_left] < m_bits[j]) {
+        m_is_small = 0;
+        break;
+      }
+    }
+    if(m_is_small) {
+      bits_sub(bits + i, 64 - m_left, m_bits + m_left, 64 - m_left);
+    } else {
+      bits_sub(bits + i, 64 - m_left + 1, m_bits + m_left, 64 - m_left);
+    }
+  }
+  uint64_t ret = bits_to_uint64(bits + 64, 64);
+  return ret;
+}
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+  assert(m != 0);
+  if(a == 0 || b == 0)
+    return 0;
+  uint8_t a_bits[64];
+  uint8_t b_bits[64];
+  uint8_t m_bits[64];
+  uint8_t tmp_bits[128];
   uint64_to_bits(a, a_bits);
   uint64_to_bits(b, b_bits);
   uint64_to_bits(m, m_bits);
-
-  return (a * b) % m; // buggy
+  uint64_t ret = 0;
+  for(int i = 0; i < 64; i ++) {
+    if(a_bits[i] == 0)
+      continue;
+    for(int j = 0; j < 128; j ++)
+      tmp_bits[j] = 0x0;
+    // tmp_bits == 00--0[multi resutl][i bits 0]
+    bit_multi_bits(a_bits[i], b_bits, tmp_bits + i + 1);
+    printf("a index: %d\n", i);
+    print_bits(b_bits, 64);
+    print_bits(tmp_bits, 128);
+    ret += bits_mod(tmp_bits, m_bits);
+    ret %= m;
+  }
+  return ret;
 }
 
 int64_t multimod_fast(int64_t a, int64_t b, int64_t m) {
