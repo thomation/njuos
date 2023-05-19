b83462c17618c6b44c62f1d1fd11ecfcc5108437
Thomation <thomation@126.com>
2023-05-19 11:48:47 +0800

    Remove init loop

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -5,26 +5,23 @@
 
 #define N 10000000
 
-static bool is_prime[N];
+static bool is_not_prime[N];
 static int  primes[N];
 
 int *sieve(int n) {
   assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
-    is_prime[i] = true;
-
   for (int i = 2; i <= n; i++) {
-    if(!is_prime[i])
+    if(is_not_prime[i])
       continue;
     for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
+      is_not_prime[j] = true;
     }
   }
 
   int *p = primes;
   *p++ = 2;
   for (int i = 3; i <= n; i+=2)
-    if (is_prime[i]) {
+    if (!is_not_prime[i]) {
       *p++ = i;
     }
   *p = 0;
