322640dc83c8b4a162b2d208c8babd2e0d5033a6
Thomation <thomation@126.com>
2023-05-21 09:21:48 +0800

    Improve cache performance

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -5,25 +5,27 @@
 
 #define N 10000000
 
-static bool is_not_prime[N];
+static __uint8_t is_not_prime[N >> 3];
 static int  primes[N];
-
+#define IS_NOT_PRIME(x)  (is_not_prime[x >> 3] & (0x1 << (x&0x7)))
+#define SET_NOT_PRIME(x) (is_not_prime[x >> 3] |= (0x1 << (x&0x7)))
 int *sieve(int n) {
   assert(n + 1 < N);
+
   for(int i = 4; i <=n; i +=2 )
-    is_not_prime[i] = true;
+    SET_NOT_PRIME(i);
   for (int i = 3; i <= n; i+= 2) {
-    if(is_not_prime[i])
+    if(IS_NOT_PRIME(i))
       continue;
     for (int j = i + i + i; j <= n; j += i + i) {
-      is_not_prime[j] = true;
+      SET_NOT_PRIME(j);
     }
   }
 
   int *p = primes;
   *p++ = 2;
   for (int i = 3; i <= n; i+=2)
-    if (!is_not_prime[i]) {
+    if (!IS_NOT_PRIME(i)) {
       *p++ = i;
     }
   *p = 0;
