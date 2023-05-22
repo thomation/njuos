6784bf2494e23ea4bb7b6abb53210ee69bf3b80b
Thomation <thomation@126.com>
2023-05-22 08:07:20 +0800

    Init array with bits mod

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,18 +2,22 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define N 10000000
 
-static __uint8_t is_not_prime[N >> 3];
+static uint8_t is_not_prime[N >> 3];
 static int  primes[N];
 #define IS_NOT_PRIME(x)  (is_not_prime[x >> 3] & (0x1 << (x&0x7)))
 #define SET_NOT_PRIME(x) (is_not_prime[x >> 3] |= (0x1 << (x&0x7)))
 int *sieve(int n) {
   assert(n + 1 < N);
-
-  for(int i = 4; i <=n; i +=2 )
-    SET_NOT_PRIME(i);
+  *is_not_prime = 0x50;
+  uint8_t *prime_end = is_not_prime + (N >> 3);
+  int index = 8;
+  for(uint8_t * q = is_not_prime + 1; q < prime_end; q++) {
+    *q = 0x55;
+  }
   for (int i = 3; i <= n; i+= 2) {
     if(IS_NOT_PRIME(i))
       continue;
