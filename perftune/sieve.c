8c1b91b3b1d190bdaffdfe5b50a44ba8c0e2928c
Thomation <thomation@126.com>
2023-05-22 08:35:26 +0800

    don't access is_not_prime with index but with cur

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -8,13 +8,14 @@
 
 static uint8_t is_not_prime[N >> 3];
 static int  primes[N];
-#define IS_NOT_PRIME(x)  (is_not_prime[x >> 3] & (0x1 << (x&0x7)))
-#define SET_NOT_PRIME(x) (is_not_prime[x >> 3] |= (0x1 << (x&0x7)))
+#define IS_NOT_PRIME(x)  (*(is_not_prime + (x >> 3)) & (0x1 << (x&0x7)))
+#define SET_NOT_PRIME(x) (*(is_not_prime + (x >> 3)) |= (0x1 << (x&0x7)))
 int *sieve(int n) {
   assert(n + 1 < N);
   *is_not_prime = 0x50;
-  uint8_t *prime_end = is_not_prime + (N >> 3);
-  int index = 8;
+  int max = n >> 3;
+  if (max << 3 < n) max ++;
+  uint8_t *prime_end = is_not_prime + max;
   for(uint8_t * q = is_not_prime + 1; q < prime_end; q++) {
     *q = 0x55;
   }
@@ -38,10 +39,24 @@ int *sieve(int n) {
 
   int *p = primes;
   *p++ = 2;
-  for (int i = 3; i <= n; i+=2)
-    if (!IS_NOT_PRIME(i)) {
-      *p++ = i;
+  *p++ = 3;
+  *p++ = 5;
+  *p++ = 7;
+  int index = 8;
+  for(uint8_t * q = is_not_prime + 1; q < prime_end; q++, index +=8) {
+    uint8_t t = *q;
+    for(int i = 1; i < 8; i += 2) {
+      int j = (t >> i) & 0x1;
+      if(j == 0) {
+        *p++ = index + i; 
+      }
     }
+  }
+  // for (int i = 3; i <= n; i+=2)
+  //   if (!IS_NOT_PRIME(i)) {
+  //     *p++ = i;
+  //   }
+
   *p = 0;
   return primes;
 }
