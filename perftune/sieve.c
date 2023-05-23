b04325f53b67d396df921719553fb58bb538787d
Thomation <thomation@126.com>
2023-05-23 09:04:19 +0800

    use 32bit array

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -5,12 +5,18 @@
 #include <stdint.h>
 
 #define N 10000000
-#define WORD_BIT 4
-#define WORD_SIZE 16 
-#define WORD_MASK 0xf
-#define PRIME_ARRAY_TYPE uint16_t
+//These macro must be updated when you change the type of prim array. 
+#define WORD_BIT 5
+#define WORD_SIZE 32 
+#define WORD_MASK 0x1f
+#define HARD_CODE_PRIME_NUM 11
+#define PRIME_ARRAY_TYPE uint32_t
+#define FIRST_PRIME_SLOT 0x55555550
+#define PRIME_SLOT 0x55555555
+//-----------------------------------------
 static PRIME_ARRAY_TYPE is_not_prime[N >> WORD_BIT];
-static int  primes[N];
+static int  primes[N] = {2,3,5,7,11,13,17,19,23,29,31};
+
 #define GET_NOT_PRIME(x) (is_not_prime + (x >> WORD_BIT)) 
 #define PRIME_BITS(x) (0x1 << (x&WORD_MASK))
 #define IS_NOT_PRIME(x) (*GET_NOT_PRIME(x) & PRIME_BITS(x))
@@ -23,12 +29,12 @@ static int  primes[N];
 }
 int *sieve(int n) {
   assert(n + 1 < N);
-  *is_not_prime = 0x5550;
+  *is_not_prime = FIRST_PRIME_SLOT;
   int max = n >> WORD_BIT;
   if (max << WORD_BIT < n) max ++;
   PRIME_ARRAY_TYPE *prime_end = is_not_prime + max;
   for(PRIME_ARRAY_TYPE * q = is_not_prime + 1; q < prime_end; q++) {
-    *q = 0x5555;
+    *q = PRIME_SLOT;
   }
 
   for (int j = 9; j <= n; j += 6) {
@@ -49,13 +55,7 @@ int *sieve(int n) {
     }
   }
 
-  int *p = primes;
-  *p++ = 2;
-  *p++ = 3;
-  *p++ = 5;
-  *p++ = 7;
-  *p++ = 11;
-  *p++ = 13;
+  int *p = primes + HARD_CODE_PRIME_NUM;
   int index = WORD_SIZE;
   for(PRIME_ARRAY_TYPE * q = is_not_prime + 1; q < prime_end; q++, index += WORD_SIZE) {
     PRIME_ARRAY_TYPE t = *q;
@@ -67,6 +67,14 @@ int *sieve(int n) {
     SELECT_RESULT(p, t, 11);
     SELECT_RESULT(p, t, 13);
     SELECT_RESULT(p, t, 15);
+    SELECT_RESULT(p, t, 17);
+    SELECT_RESULT(p, t, 19);
+    SELECT_RESULT(p, t, 21);
+    SELECT_RESULT(p, t, 23);
+    SELECT_RESULT(p, t, 25);
+    SELECT_RESULT(p, t, 27);
+    SELECT_RESULT(p, t, 29);
+    SELECT_RESULT(p, t, 31);
   }
   *p = 0;
   return primes;
