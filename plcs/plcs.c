ac6cc5e9c1e73b9a6c5bcd1e5801cf92ed7d781c
Thomation <thomation@126.com>
2023-07-11 09:37:40 +0800

    Add script of test case

--- plcs/plcs.c
+++ plcs/plcs.c
@@ -34,19 +34,40 @@ void Tworker(int id) {
   result = dp[N - 1][M - 1];
 }
 
+void raw_worker() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      // Always try to make DP code more readable
+      int skip_a = DP(i - 1, j);
+      int skip_b = DP(i, j - 1);
+      int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
+      dp[i][j] = MAX3(skip_a, skip_b, take_both);
+    }
+  }
+
+  result = dp[N - 1][M - 1];
+}
 int main(int argc, char *argv[]) {
-  // No need to change
-  assert(scanf("%s%s", A, B) == 2);
+  if(argc != 4) {
+    printf("Usage: plcs threads count a b, argc = %d\n", argc);
+    exit(1);
+  }
+  strcpy(A, argv[2]);
+  strcpy(B, argv[3]);
   N = strlen(A);
   M = strlen(B);
-  T = !argv[1] ? 1 : atoi(argv[1]);
+  T = atoi(argv[1]);
+  if(T == 1) {
+    printf("Single Thread Version\n");
+    raw_worker();
+  } else {
+    printf("Multiple Threads Version\n");
+    // Add preprocessing code here
 
-  // Add preprocessing code here
-
-  for (int i = 0; i < T; i++) {
-    create(Tworker);
+    for (int i = 0; i < T; i++) {
+      create(Tworker);
+    }
+    join();  // Wait for all workers
   }
-  join();  // Wait for all workers
-
   printf("%d\n", result);
 }
