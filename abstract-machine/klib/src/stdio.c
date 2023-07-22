71b7bbe76bed2672b798c82686deeacb2ccf12b9
Thomation <thomation@126.com>
2023-07-22 09:14:28 +0800

    update klib

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,10 +3,8 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 
-#define MAX_INT_STR_LEN 100
-
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+#define MAX_INT_STR_LEN 32
 int uint_to_str(unsigned int n, char * str)
 {
   char tmp[MAX_INT_STR_LEN];
@@ -26,6 +24,11 @@ int uint_to_str(unsigned int n, char * str)
 }
 int int_to_str(int n, char * str)
 {
+  if(n == 0) {
+    str[0] = '0';
+    str[1] = '\0';
+    return 1;
+  }
   if(n > 0)
     return uint_to_str(n, str);
   char tmp[MAX_INT_STR_LEN];
@@ -44,57 +47,133 @@ int int_to_str(int n, char * str)
   str[j] = '\0';
   return j;
 }
-int printf(const char *fmt, ...) {
-  va_list list;
-  va_start(list, fmt);
-  const char * cur = fmt;
-  int n = 0;
-  while(*cur != '\0')
+int hex_to_str(unsigned int n, char * str)
+{
+  char tmp[MAX_INT_STR_LEN];
+  int i = 0;
+  while(n > 0)
   {
-    if(*cur == '%')
-    {
-      cur ++;
-      if(*cur == 'd')
-      {
-        int i = va_arg(list, int);
-        char str[MAX_INT_STR_LEN];
-        int len = int_to_str(i, str);
-        putstr(str);
-        n += len;
-        cur ++;
-      }
-      else if(*cur == 's')
-      {
-        char* s = va_arg(list, char*);
-        putstr(s);
-        n += strlen(s);
-        cur ++;
-      }
-      else
-      {
-        char error[50] = "__ is not supported now!";
-        error[0] = '%';
-        error[1] = *cur;
-        panic(error);
+    int v = n % 16;
+    tmp[i ++] =  v >= 10 ? v - 10 + 'a' : v + '0';
+    n /= 16;
+  }
+  str[0] = '0';
+  str[1] = 'x';
+  int j = 2;
+  if(i == 0)
+    str[j++] = '0';
+  while(i > 0)
+  {
+    str[j++] = tmp[--i];
+  }
+  str[j] = '\0';
+  return j;
+}
+int str_to_int(char* str) {
+  int ret = 0;
+  while(*str != '\0')
+  {
+    ret *= 10;
+    ret += *str ++ - '0';
+  }
+  return ret; 
+}
+int handle_number(char *out, char*in, int len, char* prefix, int pi) {
+    int min_len = len;
+    char pad = ' ';
+    if(pi > 0) {
+      if(prefix[0] >= '1' && prefix[0] <= '9') {
+        min_len = str_to_int(prefix);
+      } else {
+        pad = prefix[0];
+        min_len = str_to_int(prefix + 1);
       }
     }
-    else
-    {
-      putch(*cur);
-      n ++;
-      cur ++;
+    for(int i = len; i < min_len; i ++) {
+      if(out == NULL) putch(pad); else *out++ = pad;
+    }
+    for(int i = 0; i < len; i ++)
+      if(out == NULL) putch(in[i]); else *out++ = in[i];
+    return min_len > len ? min_len : len;
+  }
+int vsprintf(char* out, const char *fmt, va_list ap) {
+  int count = 0;
+  char prefix[8];
+  while(*fmt) {
+    if(*fmt == '%') {
+      fmt ++;
+      int pi = 0;
+      while(*fmt >= '0' && *fmt <= '9') {
+        prefix[pi ++] = *fmt ++;
+      }
+      prefix[pi] = '\0';
+      switch(*fmt ++) {
+        case 'd':
+        {
+          char tmp[32];
+          int d = va_arg(ap, int);
+          int len = int_to_str(d, tmp);
+          int n = handle_number(out, tmp, len, prefix, pi);
+          count += n; 
+          if(out != NULL) out += n;
+        }
+        break;
+        case 'u':
+        {
+          char tmp[32];
+          int d = va_arg(ap, int);
+          int len = uint_to_str(d, tmp);
+          int n = handle_number(out, tmp, len, prefix, pi);
+          count += n; 
+          if(out != NULL) out += n;
+        }
+        break;
+        // x and p are same
+        case 'x':
+        case 'p':
+        {
+          char tmp[32];
+          int d = va_arg(ap, int);
+          int len = hex_to_str(d, tmp);
+          int n = handle_number(out, tmp, len, prefix, pi);
+          count += n; 
+          if(out != NULL) out += n;
+        }
+        break;
+        case 's': {
+          char *s = va_arg(ap, char*);
+          int len = strlen(s);
+          count += len;
+          for(int i = 0; i < len; i ++)
+            if(out == NULL) putch(s[i]); else *out ++ = s[i];
+        }
+        break;
+        default:
+          panic("Unsupported");
+      }
+    }
+    else{
+      if(out == NULL) putch(*fmt ++); else *out ++ = *fmt++;
+      count ++;
     }
   }
-  va_end(list);
-  return n;
+  if(out != NULL) *out = '\0';
+  return count;
 }
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(NULL, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,49 +5,85 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  int len = 0;
-  while(*s != '\0')
-  {
-    s ++;
+  size_t len = 0;
+  while(*s++ != '\0')
     len ++;
-  }
   return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char * p = dst;
+  while((*p++ = *src++) != '\0')
+    ;
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char * p = dst;
+  int i = 0;
+  while(i++ < n && (*p++ = *src++) != '\0')
+    ;
+  while(i ++ < n)
+    *p ++ = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char * p = dst;
+  while(*p++ != '\0')
+    ;
+  while((*p++ = *src++) != '\0')
+    ;
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for(; *s1 != '\0' && *s2 != '\0'; s1++, s2++) {
+    if(*s1 < *s2)
+      return -1;
+    if(*s1 > *s2)
+      return 1;
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for(int i = 0; i < n && *s1 != '\0' && *s2 != '\0'; s1++, s2++, i++) {
+    if(*s1 < *s2)
+      return -1;
+    if(*s1 > *s2)
+      return 1;
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char * p = s;
+  for(int i = 0; i < n; i ++)
+    *p ++ = c;
+  return s;
 }
-
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  panic("Not implemented, need malloc and free");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  unsigned char * d = out;
+  const unsigned char * s = in;
+  for(int i = 0; i < n; i ++)
+    *d++ = *s ++;
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char * c1 = s1;
+  const unsigned char * c2 = s2;
+  for(int i = 0; i < n; i ++) {
+    int r = (int)(*c1 - *c2);
+    if(r != 0)
+      return r;
+  }
+  return 0;
 }
 
 #endif
