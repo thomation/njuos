9c25ea4ddb71bf51f6d795c0e8ca976664f071c3
Thomation <thomation@126.com>
2023-08-08 09:19:11 +0800

    Implement spinlock

--- am-kernels/kernels/spinlock/spinlock.c
+++ am-kernels/kernels/spinlock/spinlock.c
@@ -1,22 +1,46 @@
 #include <am.h>
 #include <klib.h>
-
-int locked = 0;
-
-void lock() {
-  while (atomic_xchg(&locked, 1));
+struct cpu {
+  int ncli;
+  bool enable;
+};
+struct cpu cpus[8];
+struct cpu *mycpu() {
+  return &cpus[cpu_current()];
+}
+typedef struct spinlock{
+  int locked;
+  struct cpu *cpu;
+} spinlock_t;
+static spinlock_t mylock;
+void initlock(spinlock_t * lk) {
+  lk->locked = 0;
+  lk->cpu = NULL;
+}
+void lock(spinlock_t *lk) {
+  bool enable = ienabled();
+  iset(false);
+  if(mycpu()->ncli == 0)
+    mycpu()->enable = enable;
+  mycpu()->ncli ++;
+  while (atomic_xchg(&(lk->locked), 1));
+  lk->cpu = mycpu();
 }
 
-void unlock() {
-  atomic_xchg(&locked, 0);
+void unlock(spinlock_t *lk) {
+  atomic_xchg(&lk->locked, 0);
+  lk->cpu = NULL;
+  mycpu()->ncli --;
+  if(mycpu()->ncli == 0 && mycpu()->enable)
+    iset(mycpu()->enable);
 }
 
 Context *on_interrupt(Event ev, Context *ctx) {
   switch (ev.event) {
     case EVENT_IRQ_TIMER:
-      lock();
+      lock(&mylock);
       printf("Interrupt %d\n", cpu_current());
-      unlock();
+      unlock(&mylock);
       break;
     default:
       break;
@@ -27,15 +51,19 @@ Context *on_interrupt(Event ev, Context *ctx) {
 
 void os_main() {
   int c = cpu_current();
+  cpus[c].ncli = 0;
   iset(true);
   while (1) {
-    lock();
-    printf("Hello from CPU %d\n", c);
-    unlock();
+    lock(&mylock);
+    printf("Hello from CPU %d, ienabled %d\n", c, ienabled());
+    unlock(&mylock);
+    for(volatile int i = 0; i < 10000000; i ++ )
+      ;
   }
 }
 
 int main() {
+  initlock(&mylock);
   cte_init(on_interrupt);
   mpe_init(os_main);
 }
