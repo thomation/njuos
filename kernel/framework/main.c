0121e454c2f28d559a019c11f77f491e777f3f77
Thomation <thomation@126.com>
2023-08-10 09:14:18 +0800

    don't use teardown

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -21,13 +21,13 @@ void test_tasks() {
   kmt->create(t1, "test-thread-1", func, "1");
   kmt->create(t2, "test-thread-2", func, "2");
   kmt->create(t3, "test-thread-3", func, "3");
-  kmt->teardown(t1);
+  // kmt->teardown(t1);
 }
 int main() {
   ioe_init();
   cte_init(os->trap);
   os->init();
-  mpe_init(os->run);
   test_tasks();
+  mpe_init(os->run);
   return 1;
 }
