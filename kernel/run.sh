dfb99f7df6d97fe6a82135b1cbbd8afafad73505
Thomation <thomation@126.com>
2023-08-11 12:09:25 +0800

    smp = 4

--- kernel/run.sh
+++ kernel/run.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -serial mon:stdio -machine accel=tcg -smp 2,sockets=2,cores=4,threads=1,maxcpus=8  -drive format=raw,file=./build/kernel-x86_64-qemu
+qemu-system-x86_64 -serial mon:stdio -machine accel=tcg -smp 4,sockets=4,cores=4,threads=1,maxcpus=16 -drive format=raw,file=./build/kernel-x86_64-qemu
