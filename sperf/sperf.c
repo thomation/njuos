24e9ca4e11a75fb143043a45b49d400ae511dfc2
Thomation <thomation@126.com>
2023-08-22 08:45:54 +0800

    Add M3

---
+++ sperf/sperf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  char *exec_argv[] = { "strace", "ls", NULL, };
+  char *exec_envp[] = { "PATH=/bin", NULL, };
+  // execve("strace",          exec_argv, exec_envp);
+  // execve("/bin/strace",     exec_argv, exec_envp);
+  execve("/usr/bin/strace", exec_argv, exec_envp);
+  printf("Hello sperf\n");
+  perror(argv[0]);
+  exit(EXIT_FAILURE);
+}
