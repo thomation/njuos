015ecc1064e24c3f2f355a15b53810a7593ac580
Thomation <thomation@126.com>
2023-08-23 08:54:17 +0800

    stderr of subrpocess to pipe

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -16,15 +16,27 @@ int main(int argc, char *argv[]) {
   //   printf("%s\n", exec_argv[i]);
   // }
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  int pid = fork();
+  int pipefd[2];
+  if (pipe(pipefd) == -1) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+  pid_t pid = fork();
   if(pid < 0) {
-    perror(argv[0]);
+    perror("fork");
     exit(EXIT_FAILURE);
   } else if(pid == 0) {
+    close(pipefd[0]); // close read
+    dup2(pipefd[1], STDERR_FILENO);
     execve("strace",          exec_argv, exec_envp);
     execve("/bin/strace",     exec_argv, exec_envp);
     execve("/usr/bin/strace", exec_argv, exec_envp);
   } else {
+    close(pipefd[1]); // close write
+    char buf;
+    while(read(pipefd[0], &buf, 1) > 0) {
+      // write(STDOUT_FILENO, &buf, 1);
+    }
     wait(NULL);
     printf("finished\n");
   }
