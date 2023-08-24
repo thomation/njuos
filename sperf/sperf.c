b99747b6bf0a8992e3a1059c4ec625d782b6e92b
Thomation <thomation@126.com>
2023-08-24 09:21:36 +0800

    print stat

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,13 +8,14 @@
 #define RESERVE_ARGC 1
 #define NAME_LEN 32
 #define TIME_LEN 32
+#define NODE_COUNT 128
 
 typedef struct strace_node {
   char name[NAME_LEN];
   float time;
 } strace_node_t;
 
-static strace_node_t lines[128];
+static strace_node_t nodes[NODE_COUNT];
 static int lines_count;
 static int find_index_in_line(char * line, char c) {
   size_t n = strlen(line);
@@ -24,8 +25,22 @@ static int find_index_in_line(char * line, char c) {
   }
   return -1;
 }
+static strace_node_t * find_node_with_name(char * name) {
+  for(int i = 0; i < lines_count; i ++) {
+    if(strcmp(nodes[i].name, name) == 0) {
+      return &nodes[i];
+    }
+  }
+  return NULL;
+}
+static void add_node(char * name, float time) {
+  assert(lines_count < NODE_COUNT);
+  strace_node_t * node = &nodes[lines_count ++];
+  strcpy(node->name, name);
+  node->time = time;
+}
 static void parse_line(char * line) {
-  printf("%s\n", line);
+  // printf("%s\n", line);
   int name_end = find_index_in_line(line, '(');
   if(name_end < 0) {
     return;
@@ -35,21 +50,33 @@ static void parse_line(char * line) {
   for(int i = 0; i < name_end; i ++)
     name[i] = line[i];
   name[name_end] = '\0';
-  printf("%s\n", name);
+  // printf("%s\n", name);
   int time_start = find_index_in_line(line, '<');
-  assert(time_start >= 0);
-  time_start ++;
   int time_end = find_index_in_line(line, '>');
-  assert(time_end >= 0);
-  char time_str[TIME_LEN];
-  int time_index = 0;
-  for(int i = time_start; i < time_end; i ++) {
-    time_str[time_index ++] = line[i];
+  float time = 0.0;
+  if(time_start >= 0 && time_end >= 0) {
+    time_start ++;
+    char time_str[TIME_LEN];
+    int time_index = 0;
+    for(int i = time_start; i < time_end; i ++) {
+      time_str[time_index ++] = line[i];
+    }
+    time_str[time_index] = '\0';
+    sscanf(time_str, "%f", &time);
   }
-  time_str[time_index] = '\0';
-  float time;
-  sscanf(time_str, "%f", &time);
-  printf("%f\n", time);
+  // printf("%f\n", time);
+  strace_node_t * node = find_node_with_name(name);
+  if(node) {
+    node->time += time;
+  } else {
+    add_node(name, time);
+  }
+}
+static void print_result() {
+  for(int i = 0; i < lines_count; i ++) {
+    strace_node_t * node = &nodes[i];
+    printf("%s:%f\n", node->name, node->time);
+  }
 }
 int main(int argc, char *argv[]) {
   char **exec_argv = malloc(sizeof(char*) * (argc + RESERVE_ARGC + 1));
@@ -97,5 +124,6 @@ int main(int argc, char *argv[]) {
     }
     wait(NULL);
     printf("finished\n");
+    print_result();
   }
 }
