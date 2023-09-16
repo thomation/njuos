8e794782d03b916282ab600e588bccad4d6b96f8
Thomation <thomation@126.com>
2023-09-16 11:35:52 +0800

    create code for express

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -5,17 +5,28 @@ enum CommandType {
   FUNC,
   EXPR,
 };
+static char code[1024];
+const char * MAIN_PATH = "/tmp/crepl_main.c";
 int parse_line(char * line) {
   if(strlen(line) > 3 && line[0] == 'i' && line[1] == 'n' && line[2] == 't') {
     return FUNC;
   }
   return EXPR;
 }
+FILE * create_code_file(const char * path) {
+  FILE *p = fopen(path, "w+");
+  return p;
+}
 void handle_func(char  * line) {
   printf("Func:%s\n", line);
 }
 void handle_expr(char * line) {
   printf("Expr:%s\n", line);
+  char * code_temple = "#include<stdio.h>\n int main(){int ret = %s; printf(\"%%d\\n\", ret);return 0;}"; 
+  sprintf(code, code_temple, line);
+  FILE * p = create_code_file(MAIN_PATH);
+  fprintf(p, "%s\n", code);
+  fclose(p);
 }
 int main(int argc, char *argv[]) {
   static char line[4096];
