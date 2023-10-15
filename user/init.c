a433daf509638564691fe0d96a8ce4bd9bfa0c1b
Thomation <thomation@126.com>
2023-10-15 10:55:05 +0800

    print test string

--- user/init.c
+++ user/init.c
@@ -1,7 +1,10 @@
 #include "ulib.h"
 
+static char * text = "Hello User!";
 int main() {
   // Example:
   // printf("pid = %d\n", getpid());
+  for(int i = 0; i < 12; i ++)
+    kputc(text[i]);
   return 0;
 }
