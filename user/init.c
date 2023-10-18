f931b7091a74615c5faa4b4c84f0a5a14018bfcb
Thomation <thomation@126.com>
2023-10-18 19:41:27 +0800

    test fork

--- user/init.c
+++ user/init.c
@@ -3,7 +3,16 @@
 int main() {
   // Example:
   // printf("pid = %d\n", getpid());
-  for(const char * s = "Hello User!"; *s; s ++)
-    kputc(*s);
+  int pid = fork();
+  if(pid == 0) {
+    while(1) {
+      kputc('c');
+    }
+  }
+  else {
+    while(1) {
+      kputc('p');
+    }
+  }
   return 0;
 }
